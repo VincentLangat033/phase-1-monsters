0b664163bffe4c0898c17d17c99218477a036b6a
Vincent Langat <kimutailangat033@gmail.com>
2022-06-22 23:25:24 +0300

    commit tree 6f6f129e1fe64ad2dc02b87aae295287d3bd1861

--- js/index.js
+++ js/index.js
@@ -14,33 +14,6 @@ document.addEventListener('DOMContentLoaded', () => {
         `
         document.getElementById('monster-container').appendChild(monster)
     } 
-
-    document.getElementById('monster-form').addEventListener('submit', (e) => {
-        e.preventDefault()
-        let createdMonsterObj = {
-            name: e.target.name.value,
-            age: e.target.age.value,
-            description: e.target.description.value
-        }
-        monsterEntry(createdMonsterObj)
-        document.getElementById('monster-form').reset()
-    })
-    
-
-    function monsterEntry (obj) {
-        fetch("http://localhost:3000/monsters", {
-        method: 'POST',
-        headers:
-            {
-            "Content-Type": "application/json",
-            Accept: "application/json"
-            }, 
-        body: JSON.stringify(obj)
-        })
-        .then(res => res.json())
-        .then(data => console.log(data))
-    }
-
     document.getElementById('forward').addEventListener('click', () => {
         fetch("http://localhost:3000/monsters/?_limit=50&_page=2")
         .then(res => res.json())
